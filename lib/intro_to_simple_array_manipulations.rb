c10378c9fed9a2c3f59cf30532cf53b5a2da0cfa
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-10-24 21:51:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,7 +26,7 @@ def using_concat(array, contents)
   array.concat(contents)
 end
 
-def using_insert(element)
-array.insert(3, element)
+def using_insert(array, element)
+array.insert([3], element)
 end
 
